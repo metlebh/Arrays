fb09917e5262f2d9e5395ab1fc5530512c04cf02
metlebh <matlabyh@code.edu.az>
2023-01-15 23:20:28 +0400

    InseAllah duz olar

---
+++ Arrays/homework/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] num = new int[] { 4, 7, 2, 9, 1 };
+            //int rem = 0;
+            //for (int i = 0; i <num.Length ; i++)
+            //{
+            //    for (int a = i+1; a < num.Length; a++)
+            //    {
+            //        if (num[i]>num[a])
+            //        {
+            //            rem = num[a];
+            //            num[a] = num[i];
+            //            num[i] = rem;
+
+            //        }
+
+            //    }
+            //    Console.WriteLine(num[i]);
+            //}
+
+
+
+            //int sum = 0;
+            //int[] num = { 1, 2, 3 };
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    sum += num[i];
+            //    sum = sum / num.Length + 1;
+
+            //}
+            //Console.WriteLine(sum);
+
+
+
+            //int[] num = new int[] { 5, 4, 3, 2, 1 };
+            //for (int i = num.Length - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine(num[i]);
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
